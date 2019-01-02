134f3e87d9c082daf066449d0e5fc6bda30d2e5b
NOR <inlight.ur.life@gmail.com>
2019-01-02 03:20:04 +0000

    my first commit

---
+++ scratches/prime.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+
+def get_primes(n):
+    primes = [2]
+    l_odds = [i for i in range(3, n + 1) if i % 2 == 1]
+    for l_odd in l_odds:
+        c = 0
+        for prime in primes:
+            if l_odd % prime == 0:
+                continue
+            else:
+                c += 1
+            if c == len(primes):
+                primes.append(l_odd)
+    print(primes)
+    
+get_primes(10 ** 3)
